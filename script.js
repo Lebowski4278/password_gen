4dcbf360241aab32791624fd11e805851f5fcd38
Nikita <poganec427850@gmail.com>
2025-05-14 18:15:36 +0300

    Создали третью версию генератора

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function GeneratePassword() {
     lcase = document.getElementById("lcase");
     ucase = document.getElementById("ucase");
-    
+    scase = document.getElementById("scase");
     resultText = document.getElementById("resultText");
     let lcasesym = "abcdefghijklmnopqrstuvwxyz";
     let ucasesym = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    
+    let scasesym = "0123456789";
     let available = "";
     if (lcase.checked) {
         available = available + lcasesym;
@@ -13,7 +13,9 @@ function GeneratePassword() {
     if (ucase.checked) {
         available = available + ucasesym;
     }
-    
+    if (scase.checked) {
+        available = available + scasesym;
+    }
     if (available.length == 0) return;
     let result = "";
     for (let i = 0; i < 24; i++) {
